cdf5e364a8f29352be8aca964b94086628963967
jdwns96 <djjng3@gmail.com>
2021-07-26 21:23:51 +0900

    fist

---
+++ README.md
@@ -0,0 +1,9 @@
+사용 모듈
+
+```npm
+    npm i @craco/craco craco-alias
+    npm i node-sass
+```
+
+CRA scss5.0 버전 충돌 이슈  
+jsconfig.json CRA 경로 이슈, CRACO 처리
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { Redirect, Switch, Link, Route } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import axios from "axios";
+
+const App = () => {
+    return (
+        <>
+            <div></div>
+        </>
+    );
+};
+
+export default App;
